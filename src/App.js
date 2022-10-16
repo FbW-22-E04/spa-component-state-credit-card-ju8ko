72fc210fa067fbeabd24351cd28327f33f4e21c8
ju8ko <julia.x.kobzar@gmail.com>
2022-10-16 23:47:38 +0200

    The exercise is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>My React App</h1>
+      <CreditCard cardInfo={cardInfo} />
     </div>
   );
 }
+const cardInfo = {
+  bankName: "N26",
+  creditCardNumber: "5555 5555 5555 5555",
+  expirationDate: "12/29",
+  name: "Julia Kobzar",
+};
+const CreditCard = (props) => {
+  return (
+    <div className="credit-card">
+      <span className="bank-name">{props.cardInfo.bankName}</span>
+      <span>{props.cardInfo.creditCardNumber}</span>
+      <span className="expiration-date">{props.cardInfo.expirationDate}</span>
+      <span>{props.cardInfo.name}</span>
+    </div>
+  );
+};
 
 export default App;
